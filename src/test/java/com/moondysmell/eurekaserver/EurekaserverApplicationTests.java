523eb454fc1badece94d32f908ac37fecbb6ed40
smellhyang <dlgiddk10@gmail.com>
2022-06-19 21:33:48 +0900

    init

---
+++ src/test/java/com/moondysmell/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moondysmell.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
